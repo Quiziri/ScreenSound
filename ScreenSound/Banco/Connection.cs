67b0ff3d67ade9ea86635232ecd24b9277cc3a06
João Quiziri <quizirironchi@hotmail.com>
2024-02-26 20:46:14 -0300

    commit

---
+++ ScreenSound/Banco/Connection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Banco;
+
+internal class Connection
+{
+    private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+    public SqlConnection ObterConexao()
+    {
+        return new SqlConnection(connectionString);
+    }
+}
