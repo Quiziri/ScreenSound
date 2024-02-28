9a9788f7715eb855171ae1e7ba192ce06c047fb2
Joao Quiziri <quizirironchi@hotmail.com>
2024-02-28 18:00:15 -0300

    Add Entity Framework

--- ScreenSound/Banco/ScreenSoundContext.cs
+++ ScreenSound/Banco/ScreenSoundContext.cs
@@ -1,21 +1,17 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using Microsoft.EntityFrameworkCore;
 using ScreenSound.Modelos;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ScreenSound.Banco;
 
-internal class Connection
+internal class ScreenSoundContext : DbContext
 {
+    public DbSet<Artista> Artistas { get; set; }
+
     private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
-    public SqlConnection ObterConexao()
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        return new SqlConnection(connectionString);
+        optionsBuilder.UseSqlServer(connectionString);
     }
-
-    
 }
+ 
