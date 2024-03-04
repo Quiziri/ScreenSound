a104a840a64a2de5c6889b50b72e5bfe6d98db0f
Joao Quiziri <quizirironchi@hotmail.com>
2024-03-03 21:51:23 -0300

    Add return for name

--- ScreenSoung.API/Program.cs
+++ ScreenSoung.API/Program.cs
@@ -6,10 +6,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 var app = builder.Build();
 
-app.MapGet("/", () =>
+app.MapGet("/Artistas", () =>
 {
     var dal = new DAL<Artista>(new ScreenSoundContext());
-    return dal.Listar();
+    return Results.Ok(dal.Listar());
+});
+
+app.MapGet("/Artistas/{nome}", (string nome) =>
+{
+    var dal = new DAL<Artista>(new ScreenSoundContext());
+    var artista = dal.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
+    if (artista is null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(artista);
 });
 
 app.Run();
