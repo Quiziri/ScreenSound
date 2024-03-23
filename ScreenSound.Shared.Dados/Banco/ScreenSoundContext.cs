73586065af8b044f52fb0cde91c21fa320a05227
Joao Quiziri <quizirironchi@hotmail.com>
2024-03-23 18:02:29 -0300

    Add Genero

--- ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
+++ ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ScreenSound.Modelos;
+using ScreenSound.Shared.Modelos.Modelos;
 
 namespace ScreenSound.Banco;
 public class ScreenSoundContext: DbContext
 {
     public DbSet<Artista> Artistas { get; set; }
     public DbSet<Musica> Musicas { get; set; }
+    public DbSet<Genero> generos { get; set; }
 
     private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV0;Integrated Security=True;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
---
+++ ScreenSound.Shared.Modelos/Modelos/Genero.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Shared.Modelos.Modelos;
+public class Genero
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; } = string.Empty;
+    public string? Descrição {  get; set; } = string.Empty;
+
+    public override string ToString()
+    {
+        return $"Nome: {Nome} - Descrição: {Descrição}" ;
+    }
+}
