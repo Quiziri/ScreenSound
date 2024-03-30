7a96aca83f127b063ade30eb1dd30d62c5fc1b95
Joao Quiziri <quizirironchi@hotmail.com>
2024-03-30 02:18:30 -0300

    Add Dark Theme

--- ScreenSound.Web/Program.cs
+++ ScreenSound.Web/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using ScreenSound.Web;
 using ScreenSound.Web.Services;
 
@@ -7,7 +8,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddMudServices();
+
 builder.Services.AddTransient<ArtistaAPI>();
+builder.Services.AddTransient<MusicaAPI>();
 
 builder.Services.AddHttpClient("API", client =>
 {
