129c8e0b2f564fd665ae053ac32094a62d77c398
Joao Quiziri <quizirironchi@hotmail.com>
2024-04-01 22:00:31 -0300

    Add EditarMusica

--- ScreenSound.Web/Services/MusicaAPI.cs
+++ ScreenSound.Web/Services/MusicaAPI.cs
@@ -1,4 +1,5 @@
-﻿using ScreenSound.Web.Response;
+﻿using ScreenSound.Web.Requests;
+using ScreenSound.Web.Response;
 using System.Net.Http.Json;
 
 namespace ScreenSound.Web.Services;
@@ -11,8 +12,28 @@ public class MusicaAPI
         _httpClient = factory.CreateClient("API");
     }
 
+    public async Task<MusicaResponse?> GetMusicaPorNomeAsync(string nome)
+    {
+        return await _httpClient.GetFromJsonAsync<MusicaResponse>($"musicas/{nome}");
+    }
+
     public async Task<ICollection<MusicaResponse>?> GetMusicaAsync()
     {
         return await _httpClient.GetFromJsonAsync<ICollection<MusicaResponse>>("musicas");
+    }
+
+    public async Task AddMusicaAsync(MusicaRequest musica)
+    {
+        await _httpClient.PostAsJsonAsync("musicas", musica);
+    }
+
+    public async Task DeleteMusicaAsync(int id)
+    {
+        await _httpClient.DeleteAsync($"musicas/{id}");
+    }
+
+    public async Task UpdateMusicaAsync(MusicaRequestEdit artista)
+    {
+        await _httpClient.PutAsJsonAsync($"musicas", artista);
     }
 }
